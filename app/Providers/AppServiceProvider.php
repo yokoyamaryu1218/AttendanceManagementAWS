5d2b8a69a0ed632a3deffaaa2306e02035f4e6e1
Ryu Yokoyama <yokoyamaryu1218@yahoo.co.jp>
2022-11-05 10:58:27 +0900

    test

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,7 +24,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if(config('app.env')=='production'){
+        if(config('app.env')=='local'){
             URL::forceScheme('https');
         }
     }
